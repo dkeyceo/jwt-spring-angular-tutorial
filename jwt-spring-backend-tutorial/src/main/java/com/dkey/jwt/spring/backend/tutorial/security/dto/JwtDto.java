44278040750c3c20c5394e7ccaa6a97c76964c76
dkeyceo <dkeyceo@gmail.com>
2023-08-11 21:55:19 +0300

    Changed on front and back JwtDto and set in localstorage only token value without roles

--- jwt-spring-backend-tutorial/src/main/java/com/dkey/jwt/spring/backend/tutorial/security/dto/JwtDto.java
+++ jwt-spring-backend-tutorial/src/main/java/com/dkey/jwt/spring/backend/tutorial/security/dto/JwtDto.java
@@ -1,9 +1,5 @@
 package com.dkey.jwt.spring.backend.tutorial.security.dto;
 
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,15 +9,4 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class JwtDto {
 	 private String token;
-	    private String bearer = "Bearer";
-	    private String username;
-	    private Collection<? extends GrantedAuthority> authorities;
-		
-	    public JwtDto(String token, String username, Collection<? extends GrantedAuthority> authorities) {
-			this.token = token;
-			this.username = username;
-			this.authorities = authorities;
-		}
-	    
-	    
 }
--- jwt-spring-backend-tutorial/src/main/java/com/dkey/jwt/spring/backend/tutorial/security/jwt/JwtProvider.java
+++ jwt-spring-backend-tutorial/src/main/java/com/dkey/jwt/spring/backend/tutorial/security/jwt/JwtProvider.java
@@ -1,9 +1,12 @@
 package com.dkey.jwt.spring.backend.tutorial.security.jwt;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import com.dkey.jwt.spring.backend.tutorial.security.entity.UserPrincipal;
@@ -27,21 +30,25 @@ public class JwtProvider {
 	
 	public String generateToken(Authentication authentication) {
 		UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
-		return Jwts.builder().setSubject(userPrincipal.getUsername())
+		List<String> roles = userPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+				.collect(Collectors.toList());
+		return Jwts.builder()
+				.setSubject(userPrincipal.getUsername())
+				.claim("roles", roles)
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(new Date().getTime() + expiration * 1000))
-				.signWith(SignatureAlgorithm.HS512, secret)
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
 	}
 	
 	public String getUsernameFromToken(String token) {
-		return Jwts.parser().setSigningKey(secret).parseClaimsJws(token)
+		return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token)
 				.getBody().getSubject();
 	}
 	
 	public boolean validateToken(String token) {
 		try {
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
             return true;
         }catch (MalformedJwtException e){
             log.error("token has wrong format");
